7cd4483a6d71f897e17c9f07cd625dfb638b0d20
Eriik <Eriik@Eriick>
2023-03-01 17:28:24 -0500

    https://github.com/Eriik07/Fundamentos.git

--- src/ec/edu/intsuperior/vista/Aplicacion.java
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -13,7 +13,10 @@ import java.util.Scanner;
  */
 public class Aplicacion {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+  
+    }
+    public static void añobisiesto(){
+          Scanner sc = new Scanner(System.in);
         int año;
         System.out.println("Introduzca año: ");
         año = sc.nextInt();
@@ -22,7 +25,77 @@ public class Aplicacion {
             
         } else {
             System.out.println("No es un año bisiesto");
+   
+        }
+            
+        }
+    public static void totaldeventas(){
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el número de ventas");
+        int numVentas=sc.nextInt();
+         int sumaVentas=0;
+        for (int i=0;i<numVentas;i++){
+             System.out.println("Introduce el precio de la venta "+(i+1));
+            int venta=sc.nextInt();
+             sumaVentas=sumaVentas+venta;
+             
+        }
+           System.out.println(sumaVentas);
+     }   
+    public static void dialaboral(){
+        Scanner leer= new Scanner (System.in);
+        
+        System.out.println("Introduce un dia de la semana");
+        String dia= leer.next();
+        
+        switch(dia){
+            case "lunes":
+            case "martes":
+            case "miercoles":
+            case "jueves":
+            case "viernes":
+                System.out.println("Es un dia laboral");
+                break;
+            case "sabado":
+            case "domingo":
+                System.out.println("Es un dia de descanso");
+            default:
+                System.out.println("Introduce un dia de la semana");
         }
     }
+    public static void conversordeTemperatura(){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese la temperatura en celsius");
+        double celsius = leer.nextDouble();
+        double fahrenheit = (celsius * 9/5)+ 32;
+        System.out.println("La temperatura en Fahrenheit es"+ fahrenheit);
+        
+    }    
+    public static void CalculadoraIMC(){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese su peso en kilogramos:");
+        int peso = leer.nextInt();
+        System.out.println("Ingrese su altura en metros:");
+        int altura = leer.nextInt();
+        int imc = peso / (altura * altura);
+        System.out.println("Su IMC es" + imc);
+    }    
+    public static void ConversordeMoneda(){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad en dólares:");
+        double dolares = leer.nextDouble();
+        double tasaCambio = 19.8;
+        double pesos = dolares * tasaCambio;
+        System.out.println("$" + dolares + " dólares equivalen a $" + pesos + " pesos");
     }
-
+    public static void CalcularelAreaAltura(){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese la base del triángulo: ");
+        double base = leer.nextDouble();
+        System.out.println("Ingrese la altura del triángulo: ");
+         double altura = leer.nextDouble();
+         System.out.print(double area = 0.5 * base * altura;);
+    }
+    
+}     
+    
